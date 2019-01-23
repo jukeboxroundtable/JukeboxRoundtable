2c24611c0073d982a3c44bf24c06fc9dd0e351f5
Eitan Simler <eitan.simler@gmail.com>
2019-01-23 13:03:24 -0800

    Initial Commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+#############################
+#       Error Routes        #
+#############################
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
