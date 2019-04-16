a4a9e48815200b72b80a15c42f7e5dd53661daed
Eitan Simler <eitan.simler@gmail.com>
2019-04-15 17:38:05 -0700

    Changed to https

--- src/static/js/sockets.js
+++ src/static/js/sockets.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    let socket = io.connect('http://' + document.domain + ':' + location.port);
+    let socket = io.connect('https://' + document.domain + ':' + location.port);
     socket.on('connect', function () {
         socket.emit({data: "I'm connected!"});
     });
